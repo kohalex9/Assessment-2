587fa584d9fe145b259a0d4e93dad66a1944bfa8
kohalex9 <kohalex9@gmail.com>
2017-08-18 10:24:30 +0800

    Add a TableViewCell class file, and remove ViewController Class file

---
+++ 45_Week2Assessment - KOH_ZHAN_HWA/ProductListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProductListTableViewCell.swift
+//  45_Week2Assessment - KOH_ZHAN_HWA
+//
+//  Created by Alex Koh on 18/08/2017.
+//  Copyright © 2017 AlexKoh. All rights reserved.
+//
+
+import UIKit
+
+class ProductListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
