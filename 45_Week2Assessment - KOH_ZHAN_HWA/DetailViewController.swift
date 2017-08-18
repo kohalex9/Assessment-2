8c07093c9879a1c958f99fad581381202aa62e4a
kohalex9 <kohalex9@gmail.com>
2017-08-18 11:10:11 +0800

    Add DetailViewController class file, a new VC in storyboard and a navigation controller

---
+++ 45_Week2Assessment - KOH_ZHAN_HWA/DetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailViewController.swift
+//  45_Week2Assessment - KOH_ZHAN_HWA
+//
+//  Created by Alex Koh on 18/08/2017.
+//  Copyright © 2017 AlexKoh. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
