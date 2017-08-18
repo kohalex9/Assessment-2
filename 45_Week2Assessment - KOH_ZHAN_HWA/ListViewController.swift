a2e6b4d73232bf92639dcebb66cfa636d02e87e7
kohalex9 <kohalex9@gmail.com>
2017-08-18 10:20:08 +0800

    Add a tablView, prototype cell and few apple products photos

---
+++ 45_Week2Assessment - KOH_ZHAN_HWA/ListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ListViewController.swift
+//  45_Week2Assessment - KOH_ZHAN_HWA
+//
+//  Created by Alex Koh on 18/08/2017.
+//  Copyright © 2017 AlexKoh. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+
+}
